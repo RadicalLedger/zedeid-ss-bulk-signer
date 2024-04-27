0738850c4b75a1afe6fee5ed5781031aab172c07
Terance Edmonds <teranceedmonds20@gmail.com>
2024-04-27 08:40:03 +0530

    test cases

--- tests/assets/callback.ts
+++ tests/assets/callback.ts
@@ -1,6 +1,6 @@
 import { CallBackData } from '../../src/types/interfaces';
 
-export default function callback(error: any, data: CallBackData) {
+export default function callback(error: any, data?: CallBackData) {
     if (error) {
         // Handle error
     }
@@ -11,7 +11,7 @@ export default function callback(error: any, data: CallBackData) {
             if (result?.error) {
                 // Handle error
             } else {
-                const vc = result;
+                // const vc = result;
                 // Store the Verifiable Credentials
                 // ...
                 // Perform operations with the Verifiable Credentials
