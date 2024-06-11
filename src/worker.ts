fff94321b4e8e1a27d164291ffc2bb2031992cde
Terance Edmonds <teranceedmonds20@gmail.com>
2024-06-11 22:29:04 +0530

    vcsd lib update

--- src/worker.ts
+++ src/worker.ts
@@ -51,20 +51,13 @@ export default class Worker {
                 return new Promise(async (resolve) => {
                     try {
                         let issuerPrivateKey = await this.issuerLoader(job);
-                        console.log(this.suite);
-                        console.log(
-                            JSON.stringify({
-                                credential: credential,
-                                issuerPrivateKey,
-                                issuanceDate: this.issuanceDate,
-                                documentLoader: this.documentLoader
-                            })
-                        );
+
                         const vc: VerifiableCredential = await verifiable.credential.create({
                             credential: credential,
                             issuerPrivateKey,
                             issuanceDate: this.issuanceDate,
-                            documentLoader: this.documentLoader
+                            documentLoader: this.documentLoader,
+                            suite: this.suite
                         });
 
                         resolve({ data, vc });
